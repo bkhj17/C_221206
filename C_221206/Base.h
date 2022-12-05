263bb071d2596e114a5e8af132f3d6b6eb4d930d
SBAUser <SBAUser@DESKTOP-PKBMIKO>
2022-12-06 08:51:16 +0900

    초기 커밋.

---
+++ C_221206/Base.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stdio.h>
+
+class Base {
+public:
+	static void Run();
+};
---
+++ C_221206/Main.cpp
@@ -0,0 +1,10 @@
+//#include "HelloWorld.h"
+//#include "Variable.h"
+//#include "Operator.h"
+//#include "Conditional.h"
+//#include "Homework.h"
+
+int main() {
+	//Homework::Run();
+	return 0;
+}
